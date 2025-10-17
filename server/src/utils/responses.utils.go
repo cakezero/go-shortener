3cafde32241fd1638a36a031288c15164421d1eb
cakezero <sareahj.com@gmail.com>
2025-10-17 17:39:41 +0100

    utils to handle json responses

---
+++ server/src/utils/responses.utils.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+
+type GlobalMap map[string]any
+
+func getJSONMessage(jsonType, message string) GlobalMap {
+	switch jsonType {
+		case "e":
+			return GlobalMap{"error": message}
+		default:
+			return GlobalMap{"message": message}
+	}
+}
+
+func SendResponse(res http.ResponseWriter, responseMessage, status string, responseData ...interface{}) {
+	switch status {
+		case "b":
+			res.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(res).Encode(getJSONMessage("e", responseMessage))
+		case "e":
+			res.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(res).Encode(getJSONMessage("e", responseMessage))
+		case "u":
+			res.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(res).Encode(getJSONMessage("e", responseMessage))
+		default:
+			if len(responseData) > 1 {
+				data := getJSONMessage("", responseMessage)
+				data["data"] = responseData[0]
+				res.WriteHeader(http.StatusBadRequest)
+				json.NewEncoder(res).Encode(data)
+			} else {
+				res.WriteHeader(http.StatusOK)
+				json.NewEncoder(res).Encode(getJSONMessage("", responseMessage))
+			}
+	}
+}
