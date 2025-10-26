cee61bbbf612ddd7e4cbf1bd2c1f15d53b4169b6
cakezero <sareahj.com@gmail.com>
2025-10-26 22:45:55 +0100

    api routes adjustments

--- server/src/routes/app.routes.go
+++ server/src/routes/app.routes.go
@@ -11,13 +11,18 @@ import (
 func Routes() *mux.Router {
 	router := mux.NewRouter()
 
-	router.Handle("/", middlewares.AuthMiddleware(http.HandlerFunc(controllers.Home)))
-	router.Handle("/delete-url", middlewares.AuthMiddleware(http.HandlerFunc(controllers.DeleteUrl))).Methods("DELETE")
-	router.HandleFunc("/login", controllers.Login).Methods("POST")
-	router.HandleFunc("/logout", controllers.Logout)
-	router.HandleFunc("/register", controllers.Register).Methods("POST")
-	router.HandleFunc("/shorten", controllers.Shorten).Methods("POST")
-	router.HandleFunc("/visit-long-url", controllers.VisitLongUrl).Methods("POST")
+	// auth
+	router.HandleFunc("/api/auth/login", controllers.Login).Methods("POST")
+	router.HandleFunc("/api/auth/logout", controllers.Logout)
+	router.HandleFunc("/api/auth/register", controllers.Register).Methods("POST")
+
+	// server logic
+	router.Handle("/api/delete-url", middlewares.AuthMiddleware(http.HandlerFunc(controllers.DeleteUrl))).Methods("DELETE")
+	router.Handle("/api/delete-urls", middlewares.AuthMiddleware(http.HandlerFunc(controllers.DeleteAllUrls))).Methods("DELETE")
+	router.Handle("/api/delete-selected-urls", middlewares.AuthMiddleware(http.HandlerFunc(controllers.DeleteSelectedUrls))).Methods("DELETE")
+	router.Handle("/api/fetch-urls", middlewares.AuthMiddleware(http.HandlerFunc(controllers.FetchUrls)))
+	router.HandleFunc("/api/shorten", controllers.Shorten).Methods("POST")
+	router.HandleFunc("/api/visit-long-url", controllers.VisitLongUrl).Methods("POST")
 
 	return router
 }
