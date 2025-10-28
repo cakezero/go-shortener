2df1a4ef4280224aa45b40bc765d834f8e9f04c4
cakezero <sareahj.com@gmail.com>
2025-10-28 01:38:35 +0100

    shortUrl route setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "react-hot-toast";
 import Body from "./pages/Body";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import VisitUrl from "./pages/VisitUrl";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Body />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/:shortUrl" element={<VisitUrl />} />
       </Routes>
       <Toaster />
     </Router>      
